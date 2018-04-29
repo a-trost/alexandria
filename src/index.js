a5b95c62344128d082bdacf0800962ed8d3613e4
Alexander Trost <AlexRTrost@gmail.com>
2018-04-29 10:36:07 -0400

    Add material-ui-theme

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#8e8e8e',
+      main: '#616161',
+      dark: '#373737',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff5f52',
+      main: '#c62828',
+      dark: '#8e0000',
+      contrastText: '#fff',
+    },
+  },
+});
 
 ReactDOM.render((<BrowserRouter><App /></BrowserRouter>), document.getElementById('root'));
 registerServiceWorker();
