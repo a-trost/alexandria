ebcd20d0d23e9850dc5ccd5dcf34de18055a8519
Alexander Trost <AlexRTrost@gmail.com>
2018-04-29 10:42:17 -0400

    Add Route to Bookcase inside App

--- src/App.js
+++ src/App.js
@@ -11,13 +11,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route
+          exact
+          path="/"
+          render={props => <Bookcase {...props} books={this.state.books} />}
+        />
       </div>
     );
   }
