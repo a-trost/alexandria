b2989a050f7405494d3f0dfaf10cf96d80f75d4e
Alexander Trost <AlexRTrost@gmail.com>
2018-04-29 10:14:03 -0400

    Add bookcase component

---
+++ src/Bookcase.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+// My Components
+import ButtonAppBar from './ButtonAppBar';
+import BookList from './BookList';
+import * as BooksAPI from './BooksAPI';
+
+function Bookcase(props) {
+  return (
+  <div>
+   <ButtonAppBar/>
+   {/* Display three shelves of books: "wantToRead", "currentlyReading", "read" */}
+   <h2>Currently Reading</h2>
+    <BookList books={props.books.filter(book=>book.shelf == "currentlyReading")}/>
+    <h2>Want To Read</h2>
+    <BookList books={props.books.filter(book=>book.shelf == "wantToRead")}/>
+    <h2>Read</h2>
+    <BookList books={props.books.filter(book=>book.shelf == "read")}/>
+        </div>
+    )
+  }
+export default Bookcase;
