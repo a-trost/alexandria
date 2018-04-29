89cc223b1258bdb4ed06cef69a1fc4c7d9996eda
Alexander Trost <AlexRTrost@gmail.com>
2018-04-29 11:28:34 -0400

    Add get user and books on componentDidMount

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import * as BooksAPI from "./BooksAPI";
 // Styles
 import "./App.css";
 class App extends Component {
+  componentDidMount() {
+    try {
+      const userJson = localStorage.getItem("user");
+      const user = JSON.parse(userJson);
+      if (user) {
+        this.setState(() => ({ user }));
+      }
+    } catch (e) {
+      // Do nothing. If the data is corrupt we'll start from scratch.
+    }
     BooksAPI.getAll().then(books => this.setState({ books }));
   }
   componentDidUpdate(prevProps, prevState) {
