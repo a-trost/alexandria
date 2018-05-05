ffd55f7d444fee80b8c81e4f7efa453b7227c037
Alexander Trost <AlexRTrost@gmail.com>
2018-05-05 08:29:41 -0400

    Move state from BookButton to parent BookDisplayItem

--- src/BookButton.js
+++ src/BookButton.js
@@ -13,102 +13,32 @@ const styles = theme => ({
   }
 });
 
-const options = [
-  "Move to Shelf",
-  "Currently Reading",
-  "Want To Read",
-  "Read",
-  "None"
-];
-
-class BookButton extends React.Component {
-  state = {
-    anchorEl: null,
-  };
-  componentDidUpdate(prevProps, prevState) {
-    let currentShelf = this.props.book.shelf;
-    // Change the selectedIndex to match the current shelf for the book
-    if (currentShelf) {
-      this.shelfMap(currentShelf)
-    } else {
-      // Check to make sure we've been passed the bookShelf prop through Search
-      if (this.props.bookShelf) {
-        try { 
-          this.shelfMap(this.props.bookShelf.filter((book)=>book.id===this.props.book.id)[0].shelf)
-        }
-        catch(error) {
-          this.shelfMap("none")
-        }
-      }
-    }
-  }
-  button = undefined;
-
-  shelfMap(currentShelf){
-    if (
-      currentShelf === "currentlyReading" && 
-      this.state.selectedIndex !== 1
-  ) {
-      this.setState({ selectedIndex: 1 });
-    } else if (
-      currentShelf === "wantToRead" &&
-      this.state.selectedIndex !== 2
-    ) {
-      this.setState({ selectedIndex: 2 });
-    } else if ( 
-      currentShelf === "read" && 
-      this.state.selectedIndex !== 3
-    ) {
-      this.setState({ selectedIndex: 3 });
-    } else if (
-      currentShelf === "none" && 
-      this.state.selectedIndex !== 4
-    ) {
-      this.setState({ selectedIndex: 4 });
-    }
-  }
-
-  handleButtonOpenClick = event => {
-    this.setState({ anchorEl: event.currentTarget });
-  };
-
-  handleMenuItemClick = (event, index) => {
-    this.setState({ selectedIndex: index, anchorEl: null });
-  };
-
-  handleClose = () => {
-    this.setState({ anchorEl: null });
-  };
-
-  render() {
-    const { classes } = this.props;
-    const { anchorEl } = this.state;
-
+function BookButton(props){
     return (
-      <div className={classes.root}>
+      <div className="book-button-root">
         <Button
           variant="fab"
           color="primary"
           className="book-list-item-btn"
-          aria-owns={anchorEl ? "simple-menu" : null}
+          aria-owns={props.anchorEl ? "simple-menu" : null}
           aria-haspopup="true"
-          onClick={this.handleButtonOpenClick}
+          onClick={props.handleButtonOpenClick}
         >
           <ArrowDropDown style={{ fontSize: 36 }} />
         </Button>
         <Menu
           id="lock-menu"
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={this.handleClose}
+          anchorEl={props.anchorEl}
+          open={Boolean(props.anchorEl)}
+          onClose={props.handleClose}
         >
-          {options.map((option, index) => (
+          {props.options.map((option, index) => (
             <MenuItem
               key={option}
               disabled={index === 0}
-              selected={index === this.state.selectedIndex}
+              selected={index === props.selectedIndex}
               onClick={() =>
-                this.props.handleShelfChange(this.props.book, index, this.handleClose)
+                props.handleShelfChange(props.book, index, props.handleClose)
               }
             >
               {option}
@@ -118,7 +48,7 @@ class BookButton extends React.Component {
       </div>
     );
   }
-}
+
 
 BookButton.defaultProps = {
   selectedIndex: 1  
--- src/BookDisplayItem.js
+++ src/BookDisplayItem.js
@@ -5,7 +5,109 @@
 import React from "react";
 import BookButton from "./BookButton";
 
-function BookDisplayItem(props) {
+const options = [
+  "Move to Shelf",
+  "Currently Reading",
+  "Want To Read",
+  "Read",
+  "None"
+];
+
+class BookDisplayItem extends React.Component {
+    state = {
+      anchorEl: null,
+      selectedIndex: 0,
+      shelf: ""
+    };
+    componentDidMount() {
+      this.checkShelf();
+    }
+    componentDidUpdate() {
+      this.checkShelf();
+    }
+
+    checkShelf() {
+      let currentShelf = this.props.book.shelf;
+      // Change the selectedIndex to match the current shelf for the book
+      if (currentShelf) {
+        this.shelfMap(currentShelf)
+      } else {
+        // Check to make sure we've been passed the bookShelf prop through Search
+        if (this.props.bookShelf) {
+          try {
+            this.shelfMap(this.props.bookShelf.filter((book) => book.id === this.props.book.id)[0].shelf)
+          } catch (error) {
+            this.shelfMap("none")
+          }
+        }
+      }
+    }
+
+    shelfMap(currentShelf) {
+      if (
+        currentShelf === "currentlyReading" &&
+        this.state.selectedIndex !== 1
+      ) {
+        this.setState({
+          selectedIndex: 1
+        });
+        this.setState({
+          shelf: "Currently Reading"
+        });
+      } else if (
+        currentShelf === "wantToRead" &&
+        this.state.selectedIndex !== 2
+      ) {
+        this.setState({
+          selectedIndex: 2
+        });
+        this.setState({
+          shelf: "Want to Read"
+        });
+      } else if (
+        currentShelf === "read" &&
+        this.state.selectedIndex !== 3
+      ) {
+        this.setState({
+          selectedIndex: 3
+        });
+        this.setState({
+          shelf: "Read"
+        });
+      } else if (
+        currentShelf === "none" &&
+        this.state.selectedIndex !== 4
+      ) {
+        this.setState({
+          selectedIndex: 4
+        });
+        this.setState({
+          shelf: false
+        });
+      }
+    }
+
+    // Button events
+    handleButtonOpenClick = event => {
+      this.setState({
+        anchorEl: event.currentTarget
+      });
+    };
+
+    handleMenuItemClick = (event, index) => {
+      this.setState({
+        selectedIndex: index,
+        anchorEl: null
+      });
+    };
+
+    handleClose = () => {
+      this.setState({
+        anchorEl: null
+      });
+    };
+
+    render() {
   return (
     <div>
       <img
@@ -14,10 +116,16 @@ function BookDisplayItem(props) {
         className="book-list-item"
       />
       <BookButton
-        book={props.book}
+            book = {this.props.book}
         // To find the book's current shelf in the search feature
-        bookShelf={props.bookShelf}
-        handleShelfChange={props.handleShelfChange}
+            bookShelf = {this.props.bookShelf}
+            handleButtonOpenClick = {this.handleButtonOpenClick}
+            handleMenuItemClick = {this.handleMenuItemClick}
+            handleClose = {this.handleClose}
+            handleShelfChange = {this.props.handleShelfChange}
+            options = {options}
+            anchorEl = {this.state.anchorEl}
+            selectedIndex={this.state.selectedIndex}
       />
             {(this.state.shelf && this.props.search) && 
             <Chip 
@@ -30,5 +138,13 @@ function BookDisplayItem(props) {
     </div>
   );
 }
+        }
+        BookDisplayItem.defaultProps = {
+          selectedIndex: 0
+        }
+
+        BookButton.propTypes = {
+          selectedIndex: PropTypes.number,
+        };
 
 export default BookDisplayItem;
