e3418db99c08105858e197f478932e889b493232
Alexander Trost <AlexRTrost@gmail.com>
2018-05-10 05:39:26 -0400

    Add counters for bookshelves

--- src/components/Bookcase.js
+++ src/components/Bookcase.js
@@ -9,24 +9,27 @@ import FloatingAddButton from './FloatingAddButton'
 import Divider from 'material-ui/Divider';
 
 function Bookcase(props) {
+    const currentlyReadingBookList = props.books.filter(book=>book.shelf === "currentlyReading");
+    const wantToReadBookList = props.books.filter(book=>book.shelf === "wantToRead")
+    const readBookList = props.books.filter(book=>book.shelf === "read")
   return (
   <div>
    <ButtonAppBar userName={props.userName} />
-   <h2>Currently Reading</h2>
+   <h2>Currently Reading <span className="book-shelf-count">({currentlyReadingBookList.length})</span></h2>
     <BookDisplayGrid 
-    books={props.books.filter(book=>book.shelf === "currentlyReading")}
+    books={currentlyReadingBookList}
     handleShelfChange={props.handleShelfChange}
     />
     <Divider />
-    <h2>Want To Read</h2>
+    <h2>Want To Read <span className="book-shelf-count">({wantToReadBookList.length})</span></h2>
     <BookDisplayGrid 
-    books={props.books.filter(book=>book.shelf === "wantToRead")}
+    books={wantToReadBookList}
     handleShelfChange={props.handleShelfChange}
     />
     <Divider />
-    <h2>Read</h2>
+    <h2>Read <span className="book-shelf-count">({readBookList.length})</span></h2>
     <BookDisplayGrid 
-    books={props.books.filter(book=>book.shelf === "read")}
+    books={readBookList}
     handleShelfChange={props.handleShelfChange}
     />
     <FloatingAddButton />
