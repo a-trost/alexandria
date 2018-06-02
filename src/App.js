a615055fe7d7f1e67c299caabbae67510ccff8c7
Alexander Trost <AlexRTrost@gmail.com>
2018-06-01 21:49:52 -0400

    Prettier - code styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 // My Components
 import Bookcase from "./components/Bookcase";
 import Search from "./components/Search";
 import Welcome from "./components/Welcome";
 import * as BooksAPI from "./BooksAPI";
-import CornerSnackbar from "./components/CornerSnackbar"
+import CornerSnackbar from "./components/CornerSnackbar";
 // Styles
 import "./App.css";
 class App extends Component {
@@ -21,11 +21,11 @@ class App extends Component {
       books: [],
       searchResults: [],
       searchQuery: "",
-      snackbarOpen:false,
-      snackbarMessage:""
+      snackbarOpen: false,
+      snackbarMessage: ""
     };
   }
-  
+
   componentDidMount() {
     try {
       const userNameJson = localStorage.getItem("userName");
@@ -45,7 +45,7 @@ class App extends Component {
       localStorage.setItem("userName", json);
     }
   }
-  
+
   debounceTimeout = null;
 
   handleSearchChange(query) {
@@ -54,20 +54,20 @@ class App extends Component {
     } else {
       this.setState({ searchQuery: query });
       clearTimeout(this.debounceTimeout);
-      this.debounceTimeout = setTimeout(()=>this.search(query),400);
+      this.debounceTimeout = setTimeout(() => this.search(query), 400);
     }
   }
 
   search(query) {
     BooksAPI.search(query)
-        .then(results => {
-          if (results.error) {
-            return (results = []);
-          } else {
-            return results;
-          }
-        })
-        .then(results => this.setState({ searchResults: results }));
+      .then(results => {
+        if (results.error) {
+          return (results = []);
+        } else {
+          return results;
+        }
+      })
+      .then(results => this.setState({ searchResults: results }));
   }
 
   handleNameChange(name) {
@@ -79,20 +79,26 @@ class App extends Component {
   }
 
   fetchBookList() {
-    BooksAPI.getAll().then(books => {this.setState({ books })});
+    BooksAPI.getAll().then(books => {
+      this.setState({ books });
+    });
   }
 
-  updateStateBooks(book,shelf) {
+  updateStateBooks(book, shelf) {
     // Update the books in state first to give the user an immediate response, then update the backend.
     let newBook = book;
     let previousStateBooks = this.state.books;
-    const existingBookIndex = previousStateBooks.findIndex(comparisonBook=>comparisonBook.id===newBook.id);
+    const existingBookIndex = previousStateBooks.findIndex(
+      comparisonBook => comparisonBook.id === newBook.id
+    );
     newBook.shelf = shelf;
-    if (existingBookIndex>-1) {
-      previousStateBooks[existingBookIndex] = newBook
+    if (existingBookIndex > -1) {
+      previousStateBooks[existingBookIndex] = newBook;
     } else {
       previousStateBooks.push(newBook);
     }
+    this.setState({ books: previousStateBooks });
+  }
 
   shelfChangeStrings = {
     1: {
@@ -110,7 +116,7 @@ class App extends Component {
     4: {
       shelfName: "none",
       movedToMessage: " removed from shelves"
-  }
+    }
   };
 
   handleShelfChange(book, shelf, closeMenu) {
@@ -124,59 +130,61 @@ class App extends Component {
   }
 
   openSnackbar(book, message) {
-    this.setState({snackbarMessage:(book.title + message)});
-    this.setState({snackbarOpen:true});
+    this.setState({ snackbarMessage: book.title + message });
+    this.setState({ snackbarOpen: true });
   }
   closeSnackbar() {
-    this.setState({snackbarOpen:false})
+    this.setState({ snackbarOpen: false });
   }
 
   render() {
     return (
       <div className="App">
-      <CornerSnackbar
+        <CornerSnackbar
           open={this.state.snackbarOpen}
           message={this.state.snackbarMessage}
           onClose={this.closeSnackbar}
         />
-        <Route
-          path="/welcome"
-          render={props => (
-            <Welcome
-              {...props}
-              userName={this.state.userName}
-              handleNameChange={this.handleNameChange}
-            />
-          )}
-        />
-        <Route
-          exact
-          path="/"
-          render={props => (
-            <Bookcase
-              {...props}
-              userName={this.state.userName}
-              books={this.state.books}
-              handleShelfChange={this.handleShelfChange}
-            />
-          )}
-        />
-        <Route
-          path="/search"
-          render={props => (
-            <Search
-              {...props}
-              userName={this.state.userName}
-              books={this.state.searchResults}
-              searchQuery={this.state.searchQuery}
-              // Pass the user's book shelf to compare against the search results:
-              bookShelf={this.state.books}
-              handleSearchChange={this.handleSearchChange}
-              handleShelfChange={this.handleShelfChange}
-              search={true}
-            />
-          )}
-        />
+        <Switch>
+          <Route
+            path="/welcome"
+            render={props => (
+              <Welcome
+                {...props}
+                userName={this.state.userName}
+                handleNameChange={this.handleNameChange}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/"
+            render={props => (
+              <Bookcase
+                {...props}
+                userName={this.state.userName}
+                books={this.state.books}
+                handleShelfChange={this.handleShelfChange}
+              />
+            )}
+          />
+          <Route
+            path="/search"
+            render={props => (
+              <Search
+                {...props}
+                userName={this.state.userName}
+                books={this.state.searchResults}
+                searchQuery={this.state.searchQuery}
+                // Pass the user's book shelf to compare against the search results:
+                bookShelf={this.state.books}
+                handleSearchChange={this.handleSearchChange}
+                handleShelfChange={this.handleShelfChange}
+                search={true}
+              />
+            )}
+          />
+        </Switch>
       </div>
     );
   }
